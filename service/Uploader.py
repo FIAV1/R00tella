ad3843baf1648529e33407d3c4a7b0c7e33ad539
NicFontana <niccolo.fontana@student.unife.it>
2018-04-05 23:52:52 +0200

    Tentativo di fix all'uploader

--- service/Uploader.py
+++ service/Uploader.py
@@ -35,5 +35,6 @@ class Uploader:
 			data = os.read(self.fd, 4096)
 			readed_size = str(len(data)).zfill(5)
 			print(f'invio {readed_size} bytes')
-			self.sd.send(readed_size.encode() + data)
+			self.sd.send(readed_size.encode())
+			self.sd.send(data)
 		os.close(self.fd)
